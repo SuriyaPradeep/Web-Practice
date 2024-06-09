3ad0d3716e36447ad0ade206dadf769fefc8bbc4
Suriya <suriyapradeep2003@gmail.com>
2024-06-09 17:42:15 +0530

    add Tic-Tac-Toe

--- MovieApp/script.js
+++ MovieApp/script.js
@@ -76,7 +76,7 @@ form.addEventListener("submit", (e) => {
   const searchTerm = search.value;
   if (searchTerm && searchTerm !== "") {
     getMovies(SEARCH_API + searchTerm);
-    search.value = "";
+    search.value = searchTerm;
   } else history.go(0);
 });
 
---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './components/TicTacToe';
+
+function App() {
+  return (
+    <>
+    <TicTacToe></TicTacToe>
+    </>
+  );
+}
+
+export default App;
